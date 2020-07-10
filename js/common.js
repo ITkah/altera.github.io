f7f611f3cfdc8fc737e15f47dc87b69e6fdd5a6b
ITkah <ilatkacenko132@gmail.com>
2020-07-10 20:19:04 +0300

    fix rezina

--- js/common.js
+++ js/common.js
@@ -152,4 +152,43 @@ $(document).ready(function() {
     fixedContentPos: false
   });
 
+  $(".calculator button").on("click", function(){
+    $(".pageAvoid").addClass("pageAvoid-active");
+  });
+
+  
+  $('.slider-post-box').on(`init reInit`, function(event, slick) {
+    $('.counter').text(1 + ' / ' + slick.slideCount);
+  });
+
+  $('.slider-post-box').on(`afterChange`, function(event, slick, currentSlide, nextSlide) {
+    $('.counter').text(currentSlide + 1 + ' / ' + slick.slideCount);
+  });
+
+  $('.slider-post-box').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: true,
+  });
+
+  $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+			titleSrc: function(item) {
+				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+			}
+		}
+	});
+
 });
